fc890b85f0ba6c4bd7eb3c85f8aa8fd385d99658
Ajay <kadamajay100@gmail.com>
2025-03-29 13:03:25 +0530

    Redis Implementation

--- ProductService/src/main/java/com/Product/ProductService/Config/WebConfig.java
+++ ProductService/src/main/java/com/Product/ProductService/Config/WebConfig.java
@@ -1,5 +1,6 @@
-package com.Product.ProductService.Client;
+package com.Product.ProductService.Config;
 
+import com.Product.ProductService.Client.AuthInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -19,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
                 .addPathPatterns("/products/**") // Protect all product endpoints
-                .excludePathPatterns("/public/**"); // Allow some endpoints to be public if needed
+                .excludePathPatterns("/public/**", "/success/**", "/success*");// Allow some endpoints to be public if needed
     }
 }
 
--- ProductService/src/main/java/com/Product/ProductService/Service/ProductService.java
+++ ProductService/src/main/java/com/Product/ProductService/Service/ProductService.java
@@ -2,21 +2,30 @@ package com.Product.ProductService.Service;
 
 import com.Product.ProductService.Model.Product;
 import com.Product.ProductService.Repository.ProductRepository;
+import jakarta.persistence.EntityManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.IllegalFormatCodePointException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public ProductService(ProductRepository productRepository) {
+    private EntityManager entityManager;
+    private RedisTemplate<Long, Object> redisTemplate;
+
+    public ProductService(ProductRepository productRepository, EntityManager entityManager, RedisTemplate<Long, Object> redisTemplate) {
         this.productRepository = productRepository;
+        this.entityManager = entityManager;
+        this.redisTemplate = redisTemplate;
     }
 
     public List<Product> getAllProducts() {
@@ -24,12 +33,25 @@ public class ProductService {
     }
 
     public Product getProductById(Long id) {
-        return productRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Product not found"));
+        Product cacheProduct = (Product) redisTemplate.opsForHash().get(id, "PRODUCTS");
+        if (cacheProduct != null) {
+            return cacheProduct;
+        }
+
+        Optional<Product> product = productRepository.findById(id);
+//        redisTemplate.opsForHash().put(id, "PRODUCTS", product.get());
+        return product.map(p -> {
+            redisTemplate.opsForHash().put(id, "PRODUCTS", p);
+            return p;
+        }).orElseThrow(() -> new RuntimeException("Product not found"));
     }
 
     public Product saveProduct(Product product) {
-        return productRepository.save(product );
+
+        if (product.getCategory() != null && product.getCategory().getId() != null) {
+            product.setCategory(entityManager.merge(product.getCategory()));
+        }
+        return productRepository.save(product);
     }
 
     public void deleteProduct(Long id) {
