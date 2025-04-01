9b30f0a5a1056c1cc5e7cef313ce04e54f2bce67
Ajay <kadamajay100@gmail.com>
2025-04-01 07:56:44 +0530

    update

--- ProductService/src/main/java/com/Product/ProductService/Service/OrderService.java
+++ ProductService/src/main/java/com/Product/ProductService/Service/OrderService.java
@@ -2,18 +2,25 @@ package com.Product.ProductService.Service;
 
 import com.Product.ProductService.Dto.OrderRequestDto;
 import com.Product.ProductService.Model.Order;
+import com.Product.ProductService.Model.Product;
 import com.Product.ProductService.Repository.OrderRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final ProductService productService;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository,ProductService productService) {
         this.orderRepository = orderRepository;
+        this.productService = productService;
     }
 
     public Order createOrder(OrderRequestDto requestDto, Long userId) {
+        Product product = productService.getProductById(requestDto.getProductId());
+        if (product == null ) {
+            throw new RuntimeException("Product out of stock");
+        }
         Order order = new Order(userId, requestDto.getProductId(), requestDto.getQuantity(), requestDto.getTotalAmount(), "INITIATED");
         return orderRepository.save(order);
     }
